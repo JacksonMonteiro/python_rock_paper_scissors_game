63e069e3180b9300caeb698d29018fe7e2cea1b8
jacksonmonteiro <infor.jackson324@gmail.com>
2022-09-24 14:40:49 -0300

    comments and code fixes

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import random
 isTie = True
 
 def checkWinner(choices):
+    """
+    Check the choices values. If the player and computer chose the same thing, it's a tie. 
+    if the choices values are different, the player win or lose the game.
+
+    choices: a dictionary with two keys: 'player' and 'computer'
+    """
     global isTie
 
     print('You chose {} and the computer chose {}'.format(choices['player'], choices['computer']))
@@ -9,21 +15,21 @@ def checkWinner(choices):
     if choices['player'] == choices['computer']:
         print('It\'s a tie')
     elif choices['player'] == 'rock':
-        if choices['computer'] == 'paper':
-            isTie = False
+        if choices['computer'] == 'scissor':
             print("You won the game")
+            isTie = False
         else:
             print("You lost the game")
             isTie = False
     elif choices['player'] == 'paper':
-        if choices['computer'] =='scissor':
+        if choices['computer'] =='rock':
             print("You won the game")
             isTie = False
         else:
             print("You lost the game")
             isTie = False
     elif choices['player'] == 'scissor':
-        if choices['computer'] =='rock':
+        if choices['computer'] =='paper':
             print("You won the game")
             isTie = False
         else: 
@@ -31,6 +37,7 @@ def checkWinner(choices):
             isTie = False
 
 
+# A while loop that will keep running until the user wins or loses.
 while isTie == True:
     def getChoices(): 
         playerChoice = input('Select your choice(rock, paper, scissor): ')
@@ -42,4 +49,3 @@ while isTie == True:
         }
     
     checkWinner(getChoices())
-
